945e7eae4da9897fcf6a113491a1c7bad6bbce42
Saiful Islam <saifulislam.ruet15@gmail.com>
2022-08-28 04:20:57 +0600

    file upload

---
+++ data.js
@@ -0,0 +1,203 @@
+const MessageData =[
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ki koro',
+        personTwo:'eito apni'
+    },
+      {
+        personOne:'oh acha',
+        personTwo:'sob thik ase? porate cailen na je'
+    },  
+    {
+        personOne:'emni valo lagse na',
+        personTwo:'amk bola jai?'
+    },  
+    {
+        personOne:'nah',
+        personTwo:'bolen'
+    },
+    {
+        personOne:'ei ase r ki',
+        personTwo:'amra amrai to, jodi ektu mone hoi tho bolen'
+    },
+    {
+        personOne:'haha, ki khb tmr',
+        personTwo:'apnake bolsilam je amra ki kisu hote pari?'
+    },
+    {
+        personOne:'mane ki',
+        personTwo:'kishu na'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+      {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+      {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+      {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+      {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },  
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    },
+    {
+        personOne:'ei id te porba',
+        personTwo:'hum Ok'
+    }
+
+]
